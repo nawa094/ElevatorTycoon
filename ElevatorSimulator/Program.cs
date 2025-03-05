8cd27fd0efb1274f5b27f4be9230098cf5918c51
Nawa Mumbwe <mumbwe94@gmail.com>
2025-03-05 12:12:28 +0200

    initial commit: add dotnet console application

---
+++ ElevatorSimulator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ElevatorSimulator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
